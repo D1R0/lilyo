1f399d17837b45aee4051755bad2949d7c6b7b59
D1R0 <38256732+D1R0@users.noreply.github.com>
2018-12-04 02:03:47 +0200

    Update client.py

--- client.py
+++ client.py
@@ -33,6 +33,6 @@ async def on_message(message):
     if message.content.startswith("id:"):
         if message.author.id=="336810396340060161":
             msg=message.content.replace("id:","")
-            await bot.send_message(message.channel,"aaa\bbb")
+            await bot.send_message(message.channel,"aaa&#92;bbb")
 
 bot.run(str(os.environ.get("BOT_TOKEN")))
